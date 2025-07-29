ff001337ef0d658a63faf987d56516c459642938
arnavktis <arnavktis@gmail.com>
2025-07-29 12:47:50 +0530

    ccd

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1044,7 +1044,7 @@ const isMobile = useIsMobile();
   return (
     <div ref={containerRef} className="relative">
       {/* Section 1 - Hero Section */}
-      <div ref={sectionRef} className=" h-[450vh] 
+      <div ref={sectionRef} className=" h-[500vh] 
     sm:h-[800vh] 
     md:h-[800vh] 
     lg:h-[800vh] 
